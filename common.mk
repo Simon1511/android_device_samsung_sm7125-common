de86acf5f796ed729137c459b477122a0dc1cf94
LuK1337 <priv.luk@gmail.com>
2019-09-03 13:25:21 +0200

    sm7125: Build Trust HAL
    Change-Id: I9487a4903e1b2f896277ae87b6f85412abd9f8ed

--- common.mk
+++ common.mk
@@ -306,6 +306,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
