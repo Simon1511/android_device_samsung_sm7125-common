c58ffdb6df52db40ba1db0c6b86d30292537a3fe
Alexander Winkowski <dereference23@outlook.com>
2021-09-30 20:54:59 +0530

    sm7125: Import cleaned up powerhint
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>
    Change-Id: I3d16e41487873dd25e51f0c8c9e2ecc0642b3c42

--- common.mk
+++ common.mk
@@ -330,6 +330,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.samsung-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
