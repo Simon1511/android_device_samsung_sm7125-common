ec715ee810c7839e79a19e71b0ca7c39323150d6
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-10-12 20:46:09 +0200

    sm7125: Also build audio 2.0, 4.0 and 5.0
    *Stock does so
    
    Change-Id: I12bee4af5af366057dc71cefc5cd1999c614b905

--- common.mk
+++ common.mk
@@ -72,8 +72,15 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
