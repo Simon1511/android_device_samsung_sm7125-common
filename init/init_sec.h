19093e36288f66edf022e428b7edfcd11f8dba24
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-11-03 17:40:00 +0100

    sm7125: init: Support A525M
    Change-Id: Ib6ca8024bfb116f238c980e46694da3253bfa27d

--- init/init_sec.h
+++ init/init_sec.h
@@ -5,6 +5,7 @@
 
 enum device_variant {
     VARIANT_A525F = 0,
+    VARIANT_A525M,
     VARIANT_MAX
 };
 
@@ -18,8 +19,14 @@ static const variant international_models = {
     .codename = "a52q"
 };
 
+static const variant america_models = {
+    .model = "SM-A525M",
+    .codename = "a52q"
+};
+
 static const variant *all_variants[VARIANT_MAX] = {
-    &international_models
+    &international_models,
+    &america_models
 };
 
 #endif // INIT_SEC_H
