6133a94801c4a073c08f468794991e9a0b4a4877
Simon1511 <simon2002.schoenmackers@gmail.com>
2021-11-13 14:55:13 +0100

    sm7125: Import keylayout for DT2W
    Change-Id: I0ed685be0ede1dab35d8c583ebd86f9eab2da458

--- common.mk
+++ common.mk
@@ -196,6 +196,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung \
