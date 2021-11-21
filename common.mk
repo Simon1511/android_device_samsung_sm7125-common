c184bf712f6e84f269853de62e48c63bdcc80749
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    sm7125: Uprev audio HAL to V7
    Updated audio policy by running:
    
    hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/samsung/gta4xl-common/configs/audio/audio_policy_configuration.xml 6.0
    
    with some manual intervention.
    
    Updated audio_policy_volumes.xml from P610XXS2DUJ4 to get it validating successfully.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514

--- common.mk
+++ common.mk
@@ -63,8 +63,8 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@6.0-impl.sm7125 \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl.sm7125 \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
     audio.usb.default \
@@ -87,7 +87,8 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/audio/configs/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(COMMON_PATH)/audio/configs/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration_7_0.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
