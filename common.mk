8f201949b86bc51291d53d0332c490bbbedf4e96
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-02-20 14:21:36 +0100

    Revert "sm7125: Restore android.hidl.base@1.0 dummy lib"
    This reverts commit 65118e21895f40dc17670b886fd964c836189aea.
    
    Change-Id: I2042582e6cd5d4d1aab870c705fbde7fbd4b4168

--- common.mk
+++ common.mk
@@ -206,8 +206,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0.vendor \
     libhidltransport \
