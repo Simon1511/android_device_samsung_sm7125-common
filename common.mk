add496427fdcca97806cb16597101d13cad45b02
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-02-20 14:29:02 +0100

    sm7125: Use stock health blobs
    Change-Id: Id08d420e59612bd172378334020beb6faa5c83bf

--- common.mk
+++ common.mk
@@ -201,8 +201,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1.vendor
 
 # HIDL
 PRODUCT_PACKAGES += \
