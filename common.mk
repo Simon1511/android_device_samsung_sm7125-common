3707617171788ff9782cdc0223017c63afde9eae
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-07-18 15:57:37 +0200

    sm7125: Import camera hidl
    *From https://github.com/LineageOS/android_device_samsung_exynos9820-common/tree/lineage-19.1/hidl/camera/provider
    
    Simon1511: Don't build as 64bit-only and uprev to 2.6
    Change-Id: I0627668fba1c8ce0da67d161342871023155a159

--- common.mk
+++ common.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.6-service.sm7125 \
     android.hardware.camera.provider@2.4-legacy \
     android.hardware.camera.provider@2.5-legacy \
     camera.device@1.0-impl \
