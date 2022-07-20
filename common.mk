8b51b3267793e1f9280f2a06fa322e13a7c23d54
Christian Oder <myself5@carbonrom.org>
2022-07-20 16:55:24 +0200

    sm7125: Add basic permission file for HotwordEnrollment
    Change-Id: I3c14d28aa6d2b746f7d82469ba174648d8313866

--- common.mk
+++ common.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/sec_touchscreen.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchscreen.kl
