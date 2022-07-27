4aa24df88bbe8070254f47ed4ec9be26b200726f
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-07-27 12:31:23 +0200

    sm7125: Import libbtdevice
    This is just a copy of the lineage-19.1 branch of system/bt/.
    
    HEAD revision at the point of copying:
    
    1df53e8efc293f959bdf3e352647f1d56125050d
    Merge tag 'android-12.1.0_r11' into staging/lineage-19.1_merge-android-12.1.0_r11
    
    Change-Id: Ic53eb59b25b8b367cb1cdfb27c29c08411c75bce

--- common.mk
+++ common.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     audio.bluetooth.default \
     android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth@1.0.vendor
+    android.hardware.bluetooth@1.0.vendor \
+    libbtdevice.sm7125
 
 # Camera
 PRODUCT_PACKAGES += \
