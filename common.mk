5890d7804848c2929575ae0b0dc0a83b1ccadaf0
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-09-03 14:08:08 +0200

    sm7125-common: Set ro.vendor.qti.va_odm.support
    Change-Id: I3c7173d642d19773be077027802ef137b72564b6

--- common.mk
+++ common.mk
@@ -461,6 +461,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Prop files
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 
 # Inherit proprietary blobs
 $(call inherit-product, vendor/samsung/sm7125-common/sm7125-common-vendor.mk)
