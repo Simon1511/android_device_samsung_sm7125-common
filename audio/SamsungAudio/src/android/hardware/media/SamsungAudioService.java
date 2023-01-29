85a73ad969b21851eeb38d949b77e04d258df4f1
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-01-29 17:20:36 +0100

    [WIP] sm7125-common: Fix all of our audio issues
    * Use a script to swap commands for "voip-call speaker" and "voip-call"
    * Also correctly route A2DP phone calls to the actual bluetooth device instead of earpiece
    
    Change-Id: I1de8b2ed57b265b65cf619000e78e290c98e3d5c

---
+++ audio/SamsungAudio/src/android/hardware/media/SamsungAudioService.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware.media;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.SystemProperties;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.Handler;
+import android.media.AudioManager;
+import android.media.AudioDeviceInfo;
+
+public class SamsungAudioService extends Service {
+    private static final String TAG = "SamsungAudioService";
+    private static final boolean DEBUG = true;
+    private int communicationState = 0;
+
+    private Handler mHandler;
+    private static Runnable mRunnable;
+    private AudioManager mAudioManager;
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "SamsungAudioService Started");
+
+        mAudioManager = getSystemService(AudioManager.class);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            public void run() {
+                if (DEBUG) Log.d(TAG, "onCreate: " + TAG + " is running");
+
+                communicationState = mAudioManager.getCommunicationDevice().getType();
+
+                if (mAudioManager.getMode() == AudioManager.MODE_IN_COMMUNICATION) {
+
+                    if (communicationState == AudioDeviceInfo.TYPE_BUILTIN_EARPIECE) {
+                        Log.e(TAG, "Earpiece");
+                        SystemProperties.set("vendor.audio.earpiece.voip", "true");
+                        SystemProperties.set("vendor.audio.speaker.voip", "false");
+                    }
+
+                    if (communicationState == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
+                        Log.e(TAG, "Speaker");
+                        SystemProperties.set("vendor.audio.speaker.voip", "true");
+                        SystemProperties.set("vendor.audio.earpiece.voip", "false");
+
+                    }
+                }
+                mHandler.postDelayed(mRunnable, 100);
+            }
+        };
+
+        mHandler.postDelayed(mRunnable, 0);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
