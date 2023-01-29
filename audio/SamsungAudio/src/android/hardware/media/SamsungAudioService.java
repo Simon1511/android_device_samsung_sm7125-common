b2097634c74313a825ad8e1dddf3f8e125e9f4e4
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-01-29 17:20:36 +0100

    sm7125-common: Fix all of our audio issues
    * Use a script to swap check if we are in an A2DP phone call
    * Use an app to check if we are in a VoIP or A2DP call and pass parameters accordingly
    * Correctly route A2DP phone calls to the actual bluetooth device instead of earpiece and un-swap speaker and earpiece in VoIP calls
    
    Change-Id: I1de8b2ed57b265b65cf619000e78e290c98e3d5c

---
+++ audio/SamsungAudio/src/android/hardware/media/SamsungAudioService.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2023 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware.media;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.os.IBinder;
+import android.util.Log;
+import android.os.Handler;
+import android.media.AudioManager;
+import android.media.AudioDeviceInfo;
+import android.media.AudioSystem;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+
+public class SamsungAudioService extends Service {
+    private static final String TAG = "SamsungAudioService";
+    private static final boolean DEBUG = false;
+
+    private Handler mHandler;
+    private static Runnable mRunnable;
+    private AudioManager mAudioManager;
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "SamsungAudioService Started");
+
+        mAudioManager = getSystemService(AudioManager.class);
+
+        BluetoothManager bluetoothManger = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter adapter = bluetoothManger.getAdapter();
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            public void run() {
+                if (DEBUG) Log.d(TAG, "onCreate: " + TAG + " is running");
+
+                if (!adapter.isEnabled()) {
+                    SystemProperties.set("vendor.audio.a2dp.call", "false");
+                }
+
+                if (SystemProperties.get("vendor.audio.a2dp.call").equals("true") && mAudioManager.getMode() == AudioManager.MODE_IN_CALL && adapter.isEnabled()) {
+                    /*if (DEBUG)*/ Log.e(TAG, "setting parameters");
+                    AudioSystem.setParameters("a2dp_call=1");
+                    AudioSystem.setParameters("a2dp_call=0");
+                }
+
+                if (mAudioManager.getMode() == AudioManager.MODE_IN_COMMUNICATION) {
+
+                    if (mAudioManager.getCommunicationDevice().getType() == AudioDeviceInfo.TYPE_BUILTIN_EARPIECE) {
+                        if (DEBUG) Log.d(TAG, "Earpiece");
+                        SystemProperties.set("vendor.audio.voip.device", "earpiece");
+                        AudioSystem.setParameters("voip_call=1");
+                        AudioSystem.setParameters("voip_call=0");
+                    }
+
+                    if (mAudioManager.getCommunicationDevice().getType() == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
+                        if (DEBUG) Log.d(TAG, "Speaker");
+                        SystemProperties.set("vendor.audio.voip.device", "speaker");
+                        AudioSystem.setParameters("voip_call=1");
+                        AudioSystem.setParameters("voip_call=0");
+                    }
+                }
+                mHandler.postDelayed(mRunnable, 1000);
+            }
+        };
+
+        mHandler.postDelayed(mRunnable, 0);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
