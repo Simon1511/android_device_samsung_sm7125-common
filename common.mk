ba9a62638210f1b4946f0448a66accf8c061fbb8
Simon1511 <me@simon1511.de>
2023-08-26 17:00:57 +0200

    sm7125-common: fingerprint: switch to hardware/samsung HAL
    Change-Id: Ic048efcf18e07694fede4d90859663c391a1a02d

--- common.mk
+++ common.mk
@@ -173,7 +173,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service-samsung.sm7125
+    android.hardware.biometrics.fingerprint@2.3-service.samsung
 
 # fastbootd
 PRODUCT_PACKAGES += \
