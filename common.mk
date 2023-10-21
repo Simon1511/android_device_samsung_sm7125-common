45dfa774875a0f9ff62fb7b2ac9de5d48f273aeb
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    sm7125-common: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- common.mk
+++ common.mk
@@ -373,6 +373,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.samsung-multihal \
     android.hardware.sensors@2.0-ScopedWakelock.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
     sensors.samsung
 
 PRODUCT_COPY_FILES += \
