64b507b1cfb4fde4f60d3feb45523adeda7683f9
Simon1511 <me@simon1511.de>
2023-11-01 13:02:37 +0100

    sm7125-common: audio: add dedicated service to pass g_call_sim_slot
    This is a partial revert of c31482154b32f70963c909f2cff590282c30cf68
    
    Change-Id: Ibba1b588ee5836e8f51dfdb96231da66d7f3b09a

--- audio/impl/include/core/default/Device.h
+++ audio/impl/include/core/default/Device.h
@@ -173,8 +173,6 @@ struct Device : public IDevice, public ParametersUtil {
 
     uint32_t version() const { return mDevice->common.version; }
 
-    int halSetParameters(const char* keysAndValues) override;
-
   private:
     bool mIsClosed;
     audio_hw_device_t* mDevice;
@@ -193,6 +191,7 @@ struct Device : public IDevice, public ParametersUtil {
 
     // Methods from ParametersUtil.
     char* halGetParameters(const char* keys) override;
+    int halSetParameters(const char* keysAndValues) override;
 };
 
 }  // namespace implementation
