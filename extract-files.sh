8fb30f395f43cd086c79764f0a0f3d33d4855547
Simon1511 <me@simon1511.de>
2024-03-17 12:43:36 +0100

    sm7125-common: switch to U RIL
    - Pull RIL and QMI blobs from A525FXXU6EWL3 (U)
    - Build sehradiomanager for working signal strength
    - Update SMSC and slotID patches accordingly
    - Build vendor.samsung.hardware.radio@2.[0,1,2] from source
    
    Change-Id: I5ee7e727133d421cc0d37eeacf647669cac2561b

--- extract-files.sh
+++ extract-files.sh
@@ -62,9 +62,9 @@ function blob_fixup() {
     case "${1}" in
         vendor/lib64/libsec-ril.so|vendor/lib64/libsec-ril-dsds.so)
             # Replace SlotID prop
-            sed -i 's/ril.dds.call.slotid/vendor.calls.slotid/g' "${2}"
-            # Pass an empty value to SecRil::RequestComplete in OnGetSmscAddressDone
-            xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aae30314aa/600e40f9820c805224008052e10315aa030080d2/g" | xxd -r -p > "${2}".patched
+            sed -i 's/ril.dds.call.ongoing/vendor.calls.slot_id/g' "${2}"
+            # Pass an empty value to SecRil::RequestComplete in OnGetSmscAddressDone (mov x3,x20 -> mov,x3,#0x0)
+            xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aa080040f9e30314aa/600e40f9820c805224008052e10315aa080040f9030080d2/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
             ;;
         vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
