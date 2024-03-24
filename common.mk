8f2459485c0af5df07b2dd6e122bb8d783eb3814
Simon1511 <me@simon1511.de>
2024-03-24 12:45:43 +0100

    Revert "sm7125-common: audio: add dedicated service to pass g_call_sim_slot"
    This reverts commit 64b507b1cfb4fde4f60d3feb45523adeda7683f9.
    
    Additionally, stop using delays as that can cause audioserver to crash
    
    Change-Id: If77eef84d04039b79ea1390dc81f89e5f01f354f

--- common.mk
+++ common.mk
@@ -72,8 +72,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener \
-    audio_parameter_service.samsung_sm7125
+    libvolumelistener
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/audio/configs/audio_configs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_configs.xml \
