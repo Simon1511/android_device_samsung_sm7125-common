fd2f811b316b6c7f9ea1db2a1beee16a084c252e
Tim Zimmermann <tim@linux4.de>
2024-05-09 06:48:24 +0200

    sm7125-common: Use pixel power HAL directly
    * Samsung one is outdated and we didn't do any meaningful changes there
    
    Change-Id: I65378563396d12ee76e7f2b34105988c7e66cf38

--- common.mk
+++ common.mk
@@ -330,7 +330,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.samsung-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     android.hardware.power@1.2.vendor
 
 PRODUCT_COPY_FILES += \
@@ -445,8 +445,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(COMMON_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/samsung \
-    hardware/samsung/aidl/power-libperfmgr
+    hardware/samsung
 
 # Prop files
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
