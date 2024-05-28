01b81581473529fad035c5b75567476bd65e42d6
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    sm7125-common: Allow to access bootable/deprecated-ota targets
    Exynos9820 is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to exynos9820-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: Ruchit <risenid@duck.com>

--- common.mk
+++ common.mk
@@ -386,6 +386,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# Update
+PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti \
