ebec17b03f6e69553776a84f345f771a2e89235d
Tim Zimmermann <tim@linux4.de>
2024-05-29 10:23:48 +0200

    sm7125-common: Switch to AIDL camera HAL
    Change-Id: Idc9ccc33c4ba9c2c6668d145405da623cf4dfc49

--- common.mk
+++ common.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.5-service.samsung \
+    android.hardware.camera.provider-service.samsung \
     libgrallocusage.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
 
