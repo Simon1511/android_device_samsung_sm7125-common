ff8155f5f3fb20bf876904c5b4478bf81b1ff66f
Ruchit <risenid@duck.com>
2024-09-11 22:06:34 +0530

    sm7125-common: Also patch libwvhidl with libcrypto-v33.so
    Change-Id: I731167a3dbd2c6d0b211333cb691ee2cb4594a11
    Signed-off-by: Ruchit <risenid@duck.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,7 +71,7 @@ function blob_fixup() {
             # Replace libutils with vndk30 libutils
             "${PATCHELF}" --replace-needed libutils.so libutils-v30.so "${2}"
             ;;
-        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
+        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so|vendor/lib/libwvhidl.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
     esac
