27d0cd706ab4e0a156386d4ffc91ef390e089726
Ruchit <risenid@duck.com>
2024-09-30 15:17:34 +0530

    sm7125-common: Build libartpalette
    F linker  : CANNOT LINK EXECUTABLE "/apex/com.android.art/bin/dex2oat64": library "libartpalette.so" not found: needed by main executable
    
    Change-Id: Icad8b6468c6ef43e6c8ac3b49705b6f9d6e5e77b
    Signed-off-by: Ruchit <risenid@duck.com>

--- common.mk
+++ common.mk
@@ -454,6 +454,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/samsung
 
+# libartpalette
+PRODUCT_HOST_PACKAGES += \
+    libartpalette
+
 # Prop files
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
