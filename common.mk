07a71eeb0ad95851a2de19b4a5c9df1713c46fb2
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-10-26 20:27:22 +0200

    sm7125-common: Drop gatekeeper rc file
    Change-Id: Idfb715cf0dba22265c352793c587b5cfe1c234f3

--- common.mk
+++ common.mk
@@ -199,9 +199,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/init/android.hardware.gatekeeper@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.gatekeeper@1.0-service.rc
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1.vendor
