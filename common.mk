048fa2e9485c68f8d526728a24e3993ba1329011
johnmart19 <johnivan19999@gmail.com>
2024-11-03 06:26:03 +0200

    sm7125-common: Update QTI USB to android.hardware.usb-service.qti
    Change-Id: Icf49d703304e8677e152d32307c257ed4c175a21
    Signed-off-by: Ruchit <risenid@duck.com>

--- common.mk
+++ common.mk
@@ -392,7 +392,7 @@ PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
+    android.hardware.usb-service.qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
