515de209a733b995f67840502dfc5e7a3ec26a81
Simon1511 <me@simon1511.de>
2025-01-01 13:21:29 +0100

    sm7125-common: setup ELF checks
    ELF checks currently disabled for
    unihal_main@2.15.so
    due to missing symbol _ZN7android19GraphicBufferMapper6unlockEPK13native_handle
    
    Change-Id: I47d1cbce6620a0672ec83f899e9569ed72f31f13

--- extract-files.sh
+++ extract-files.sh
@@ -67,7 +67,7 @@ function blob_fixup() {
             xxd -p -c0 "${2}" | sed "s/600e40f9820c805224008052e10315aa080040f9e30314aa/600e40f9820c805224008052e10315aa080040f9030080d2/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
             ;;
-        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
+        vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so|vendor/lib64/libkeymaster_helper.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
         vendor/lib/libwvhidl.so|vendor/lib/mediadrm/libwvdrmengine.so)
