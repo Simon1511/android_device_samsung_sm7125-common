a61bf5b78c1dc117f01c736f3cea51ab6cc65fa9
basamaryan <basam.aryan@gmail.com>
2025-01-09 15:56:49 -0500

    sm7125-common: media: Import Codec2 configs
    From sunfish TQ3A.230805.001.S1
    
    Change-Id: Ie2ecfa912e0fe0892a2e6b8ddc3a6b6aa3213d40

--- common.mk
+++ common.mk
@@ -235,6 +235,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
+    $(COMMON_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(COMMON_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml \
