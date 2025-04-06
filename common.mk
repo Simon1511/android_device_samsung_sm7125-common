251648044f534a99fca81f55a0221f95b4278f27
Simon1511 <me@simon1511.de>
2025-04-06 12:48:50 +0200

    sm7125-common: drop uneeded 64bit blobs
    Change-Id: Ib26def190f853df71b60c62163e5ca20dd9bfb7d

--- common.mk
+++ common.mk
@@ -326,6 +326,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
     libsqlite.vendor
 
 # Radio
