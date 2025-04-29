58ef50afab70ebd1c5272016b0dd8c6d90ea136a
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sm7125-common: Migrate Lineage Health to soong_config_set
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- common.mk
+++ common.mk
@@ -182,6 +182,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 # FlipFlap
 PRODUCT_PACKAGES += \
     FlipFlap
